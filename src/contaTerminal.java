44e09bb6dd17bffde8cad8faf62c871719a3b144
JefersonMatos09 <decoo.matos@live.com>
2023-09-21 11:32:17 -0300

    projeto conta banco

--- src/contaTerminal.java
+++ src/contaTerminal.java
@@ -1,5 +1,18 @@
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("meu nome é deco");
+         String nome = "Jeferson Matos";
+         double saldo = 5235.22;
+         int conta = 2510;
+         String banco = "Santander"; 
+         
+         System.out.println("Por favor Insira seu nome:");
+
+        System.out.println("Por favor digite o numero da sua conta:");
+
+        System.out.println("A sua agencia é o Banco: ");
+
+        System.out.println("Olá "+ nome + ",obrigado por criar uma conta em nosso banco,sua agencia é "+ banco + ",conta: "+ conta + ",e seu saldo: "+ saldo +",ja esta disponivel para saque."  );
+
+
     }
 }
